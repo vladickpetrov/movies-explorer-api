38f7091e620aaa3482bdef8dda37de087a050132
vladickpetrov <99147671+vladickpetrov@users.noreply.github.com>
2023-02-01 16:01:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ const limiter = rateLimit(limitSettings);
 
 const options = {
   origin: [
-    'http://localhost:3000',
     'https://videovlad.nomoredomains.club',
     'http://videovlad.nomoredomains.club',
   ],
